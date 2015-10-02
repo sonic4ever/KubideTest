3adcd778ac2f3cbe61cb999d79c39c0f49ad3583
sonic4ever <sonic4ever@hotmail.com>
2015-10-02 20:04:55 +0200

    Adecuación inicial a las especificaciones
    Se hace una adecuación inicial al uso de objetos como se pide en las
    especificaciones

--- enumeracion.php
+++ enumeracion.php
@@ -6,12 +6,17 @@
 		<h1>Listado de la clase</h1>
 		<ol type="1">
 			<?php 
+				class palabra{
+					public $tres = 'Foo';
+					public $cinco = 'Bar!';
+				}
+				$palabras = new palabra();
 				for ($numAlumnos=1;$numAlumnos<=100;$numAlumnos++){
 					if ($numAlumnos%3==0){
-						echo '<li>Foo</li>';
+						echo '<li>'.$palabras->tres.'</li>';
 					}else{
 						if ($numAlumnos%5==0){
-							echo '<li>Bar!</li>';
+							echo '<li>'.$palabras->cinco.'</li>';
 						}else{
 								echo '<li>'.$numAlumnos.'</li>';
 						}
